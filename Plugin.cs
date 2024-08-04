ea4cb79da220af337470327afb64cdf861faa9b9
peinwastaken <87280510+peinwastaken@users.noreply.github.com>
2024-08-04 17:12:07 +0300

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,57 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using shadowflickerfix.patches;
+using System;
+using UnityEngine;
+
+namespace shadowflickerfix
+{
+    public enum EShadowCascades
+    {
+        LOW = 2,
+        HIGH = 4,
+    }
+
+    [BepInPlugin("com.pein.shadowflickerfix", "Shadow Flicker Fix", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ConfigEntry<DistantShadow.ResolutionState> ResolutionState { get; set; }
+        public static ConfigEntry<EShadowCascades> ShadowCascades { get; set; }
+
+        public static DistantShadow GetDistantShadow()
+        {
+            Camera camera = Camera.main;
+            return camera.GetComponentInChildren<DistantShadow>();
+        }
+
+        public static void OnUpdateSettings(object sender, EventArgs args)
+        {
+            DistantShadow distantShadow = GetDistantShadow();
+            if (distantShadow != null)
+            {
+                distantShadow.CurrentMaskResolution = ResolutionState.Value;
+                QualitySettings.shadowCascades = (int)ShadowCascades.Value;
+            }
+        }
+
+        private void Awake()
+        {
+            new GameStartPatch().Enable();
+
+            ResolutionState = Config.Bind("General", "Distant Shadow Resolution", DistantShadow.ResolutionState.FULL, new ConfigDescription(
+                "Changes the resolution of distant shadows. Higher values reduce flickering but come with a small performance impact.",
+                null,
+                new ConfigurationManagerAttributes { Order = 100 }
+            ));
+
+            ShadowCascades = Config.Bind("General", "Shadow Cascades", EShadowCascades.LOW, new ConfigDescription(
+                "Changes the amount of shadow cascades. Makes nearby shadows look nicer. I didn't notice a performance impact but if you do keep it at the default value.",
+                null,
+                new ConfigurationManagerAttributes { Order = 95 }
+            ));
+
+            ResolutionState.SettingChanged += OnUpdateSettings;
+            ShadowCascades.SettingChanged += OnUpdateSettings;
+        }
+    }
+}
---
+++ patches/DistantShadowPatches.cs
@@ -0,0 +1,22 @@
+﻿using Comfort.Common;
+using EFT;
+using SPT.Reflection.Patching;
+using System.Reflection;
+using static DistantShadow;
+
+namespace shadowflickerfix.patches
+{
+    public class GameStartPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(GameWorld).GetMethod(nameof(GameWorld.OnGameStarted));
+        }
+
+        [PatchPostfix]
+        private static void PatchPostfix(GameWorld __instance)
+        {
+            Plugin.OnUpdateSettings(null, null);
+        }
+    }
+}
